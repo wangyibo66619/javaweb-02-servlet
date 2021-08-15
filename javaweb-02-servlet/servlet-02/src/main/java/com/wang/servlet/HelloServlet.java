48f4b5a5019581844c0c39fe1ee81296817663c3
wangyibo <2375847849@qq.com>
2021-08-15 22:35:21 +0800

    Add files via upload

---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/wang/servlet/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.wang.servlet;
+
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        String username = "王艺博";
+
+        servletContext.setAttribute("username",username);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
