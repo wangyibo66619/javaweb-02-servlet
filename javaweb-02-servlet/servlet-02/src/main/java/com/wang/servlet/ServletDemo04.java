646d70ae1156d7722feb899e3f37defaa43ca20e
wangyibo <2375847849@qq.com>
2021-08-16 15:15:14 +0800

    Add files via upload

---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/wang/servlet/ServletDemo04.java
@@ -0,0 +1,25 @@
+package com.wang.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo04 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        //RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/gp");// 转发的请求路径
+        //requestDispatcher.forward(req,resp);  // 调用forward实现请求转发
+        servletContext.getRequestDispatcher("/gp").forward(req,resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/wang/servlet/ServletDemo05.java
@@ -0,0 +1,29 @@
+package com.wang.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ServletDemo05 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        InputStream is = this.getServletContext().getResourceAsStream("/WEB-INF/classes/db.properties");
+        Properties properties = new Properties();
+        properties.load(is);
+        String user = properties.getProperty("username");
+        String pwd = properties.getProperty("password");
+
+        resp.getWriter().print(user + ":" + pwd);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
